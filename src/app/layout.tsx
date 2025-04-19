9b80546b9047bcdb46c4ee13f975fb637c1c3729
PhirunOem <phirunoem99@gmail.com>
2025-04-19 20:40:01 +0700

    Add project logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import { getServerUser } from "@/lib/auth";
 import { AuthProvider } from "@/context/AuthContext";
 import "./globals.css";
 
+export const metadata = {
+  title: "Your Mindset",
+  description: "Show you about the motivation.",
+  icons: {
+    icon: "/logo.svg",
+  },
+};
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const user = await getServerUser();
 

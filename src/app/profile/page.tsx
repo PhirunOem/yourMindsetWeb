4f02d1285ed5d26d0f571914a02de6bd9842db92
PhirunOem <phirunoem99@gmail.com>
2025-04-23 20:15:57 +0700

    Correct image path

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,11 +3,12 @@
 import Image from "next/image";
 
 import { useAuth } from "@/context/AuthContext";
-import coverImage from '@/assets/images/cover.png'
+import arrowBackIcon from '@/assets/svgs/arrowBackIcon.svg'
 import ProfileAvartar from "@/components/profile/profileAvartar";
 import Button from "@/components/button/Button";
 import { useRouter } from "next/navigation";
 import { Logout } from "@/actions/logout";
+import Link from "next/link";
 
 export default function ProfilePage() {
     const { user } = useAuth();
@@ -17,30 +18,23 @@ export default function ProfilePage() {
         window.location.href = '/';
     }
     return <div>
-        <div className="relative justify-center flex text-center content-center items-center">
-            <div className="absolute">
-                <Image src={coverImage} alt={""} />
-            </div>
-            <div className="absolute top-25 left-20 max-md:top-3 max-md:left-5">
-                <ProfileAvartar userName={user?.name} className="w-[100px] h-[100px] border-[3px] border-white max-md:w-[60px] max-md:h-[60px]" />
-            </div>
-            <div className="absolute top-40 p-10 max-md:top-8 w-full max-md:p-8">
-                <div className="flex justify-between">
-                    <div className="flex items-center gap-4">
-                        <Button title="Edit Profile" className="rounded-[5px]" />
-                        <div>
-                            <h2 className="font-bold text-lg">{user?.name}</h2>
-                            <p>{user?.email}</p>
-                        </div>
-                    </div>
-                    <div>
-                        <Button title="Log out" className="bg-red-500 p-2 rounded-[5px]" onClick={handleSignOutClick} />
-                    </div>
+        <div className="p-8">
+            <Link href={'/'}>
+                <Image src={arrowBackIcon} width={30} height={30} alt={""} />
+            </Link>
+            <div className="w-full justify-center items-center flex mt-8 gap-8">
+                <div >
+                    <ProfileAvartar userName={user?.name} className="w-[100px] h-[100px] border-[3px] border-white max-md:w-[60px] max-md:h-[60px]" />
+                </div>
+                <div className="border-r-[2px] w-[2px] h-full">
+
+                </div>
+                <div>
+                    <p className="font-bold text-2xl">{user?.name}</p>
+                    <p>{user?.email}</p>
+
                 </div>
             </div>
-        </div>
-        <div>
-            <h2>No post</h2>
         </div>
     </div>
 }
